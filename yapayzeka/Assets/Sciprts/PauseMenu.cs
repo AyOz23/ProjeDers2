9fe018fc6dde9058a372945b95590745413b98aa
Üzeyir Cenker Arda <202104115040@dogus.edu.tr>
2024-01-29 20:17:59 +0300

    CenkDate V6

---
+++ yapayzeka/Assets/Sciprts/PauseMenu.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    private Button returnButton;
+    private Button settingsButton;
+    private Button quitButton;
+  [SerializeField]  private GameObject pauseMenuUI;
+    public static bool GameIsPaused = false;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+
+    }
+    
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        Debug.Log("truu");
+    }
+}
